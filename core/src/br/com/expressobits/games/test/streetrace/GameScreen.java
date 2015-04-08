685b42d4fac0e7c1537aad51d37e978a9ba639df
Rafael Correa <rafinha2108@gmail.com>
2015-04-08 15:32:53 -0300

    Adição da tela de jogo

---
+++ core/src/br/com/expressobits/games/test/streetrace/GameScreen.java
@@ -0,0 +1,119 @@
+package br.com.expressobits.games.test.streetrace;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.input.GestureDetector;
+import com.badlogic.gdx.input.GestureDetector.GestureListener;
+import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.utils.viewport.StretchViewport;
+
+public class GameScreen implements Screen, GestureListener {
+
+	private Stage stage;
+	private TrafficGame trafficGame;
+	
+	public GameScreen() {
+		stage = new Stage();
+		trafficGame = new TrafficGame();
+		stage.addActor(trafficGame);
+	}
+	
+	@Override
+	public boolean touchDown(float x, float y, int pointer, int button) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean tap(float x, float y, int count, int button) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean longPress(float x, float y) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean fling(float velocityX, float velocityY, int button) {
+		if(velocityY < -100) trafficGame.playerCar.tryMoveUp();
+		if(velocityY > 100) trafficGame.playerCar.tryMoveDown();
+		return false;
+	}
+
+	@Override
+	public boolean pan(float x, float y, float deltaX, float deltaY) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean panStop(float x, float y, int pointer, int button) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean zoom(float initialDistance, float distance) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean pinch(Vector2 initialPointer1, Vector2 initialPointer2,
+			Vector2 pointer1, Vector2 pointer2) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public void show() {
+		Gdx.input.setInputProcessor(new GestureDetector(this));
+	}
+
+	@Override
+	public void render(float delta) {
+		Gdx.gl.glClearColor(0,0,0,1);
+		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+		stage.act(delta);
+		stage.draw();
+		
+	}
+
+	@Override
+	public void resize(int width, int height) {
+		stage.setViewport(new StretchViewport(width, height));
+		
+		stage.getCamera().translate(stage.getWidth(),stage.getHeight(),0);
+		
+
+	}
+
+	@Override
+	public void pause() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void resume() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void hide() {
+		Gdx.input.setInputProcessor(null);
+	}
+
+	@Override
+	public void dispose() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
