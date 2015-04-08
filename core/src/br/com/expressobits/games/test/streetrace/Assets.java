dea78e4a92c506da6733a7926c6a4c8a84cdedfd
Rafael Correa <rafinha2108@gmail.com>
2015-04-08 14:58:48 -0300

    Adição do gerenciador de ativos.
    Classe estática que armazena um  textureAtlas com texturas do jogo.

--- core/src/br/com/expressobits/games/test/streetrace/Assets.java
+++ core/src/br/com/expressobits/games/test/streetrace/Assets.java
@@ -1,5 +1,29 @@
 package br.com.expressobits.games.test.streetrace;
 
-public class Assets {
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.g2d.TextureAtlas;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.utils.Disposable;
 
+/**
+ * Classe que gerencia os ativos do jogo.
+ * Bom exemplo de gerenciamento de ativos separados da lógica
+ * do jogo.com um simples método dispose
+ * @author Rafael
+ *
+ */
+public class Assets{
+	public static TextureAtlas atlas;
+	public static TextureRegion car;
+	public static TextureRegion road;
+	
+	public static void load() {
+		atlas = new TextureAtlas(Gdx.files.internal("images.atlas"));
+		car = atlas.findRegion("car");
+		road = atlas.findRegion("road");
+	}
+	
+	public static void dispose(){
+		atlas.dispose();
+	}
 }
